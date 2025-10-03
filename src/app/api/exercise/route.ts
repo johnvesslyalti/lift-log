0f4c7f2a361e8579090ef8240c1f915b5d8eb9a2
johnvesslyalti <altijohnvessly@gmail.com>
2025-10-03 19:42:18 +0530

    feat: added route.ts for the exercise

--- src/app/api/exercise/route.ts
+++ src/app/api/exercise/route.ts
@@ -1,7 +1,8 @@
 import { auth } from "@/lib/auth";
+import { exerciseSchema } from "@/lib/validation";
 import { PrismaClient } from "@prisma/client";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod";
 
 const prisma = new PrismaClient();
@@ -16,11 +17,10 @@ export async function GET() {
 
   try {
     const exercises = prisma.exercises.findMany({
-        where: { userId: session.user.id }
-    })
+      where: { userId: session.user.id },
+    });
 
-    return NextResponse.json(exercises, { status: 200 })
-
+    return NextResponse.json(exercises, { status: 200 });
   } catch (error: unknown) {
     if (error instanceof ZodError) {
       return NextResponse.json({ error: error.issues }, { status: 401 });
@@ -32,6 +32,29 @@ export async function GET() {
   }
 }
 
-export async function POST() {
+export async function POST(req: NextRequest) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session)
+    return NextResponse.json({ message: "Invalid User" }, { status: 401 });
+  try {
+    const body = await req.json()
+
+    const validateData = await exerciseSchema.parse(body)
+
+    await prisma.exercises.create({
+        data: {
+            userId: session.user.id,
+            validateData
+        }
+    })
     
+  } catch (error: unknown) {
+    if (error instanceof ZodError) {
+      return NextResponse.json({ error: error.issues }, { status: 401 });
+    } else if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("unexpected error", error);
+    }
+  }
 }
