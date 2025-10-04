eeb80bef9ac9a611ea71179850f5e88e2d85722e
johnvesslyalti <altijohnvessly@gmail.com>
2025-10-04 11:35:45 +0530

    feat: Created zustand

---
+++ src/store/userStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+
+interface User {
+    id: string,
+    name: string,
+    email: string,
+    image?: string,
+    height?: number,
+    weight?: number,
+}
+
+interface UserStore {
+    user: User | null,
+    setUser: (user: User) => void;
+    clearUser: () => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+    user: null,
+    setUser: (user) => set({ user }),
+    clearUser: () => set({ user: null })
+}))
