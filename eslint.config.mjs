88734d977a69a78d800aaa9280c7ab3605760d5e
johnvesslyalti <altijohnvessly@gmail.com>
2025-10-14 19:30:35 +0530

    feat: update ESLint configuration to ignore node_modules and generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,8 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "prisma/generated/**",   // <- Add this
+      "src/generated/**",      // <- Add this
     ],
   },
 ];
