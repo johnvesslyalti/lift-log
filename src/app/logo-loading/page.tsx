5ddc19dab478d8bcd03630ff3e542d393d140f61
johnvesslyalti <altijohnvessly@gmail.com>
2025-11-05 12:58:39 +0530

    feat: upgrade with the logo loading

---
+++ src/app/logo-loading/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { motion } from "motion/react";
+
+export default function LogoLoading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{
+          opacity: 1,
+          scale: [1, 1.12, 1],
+        }}
+        transition={{
+          duration: 1.4,
+          repeat: Infinity,
+          ease: "easeOut",
+        }}
+      >
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 64 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              id="liftlogGradient"
+              x1="0"
+              y1="0"
+              x2="64"
+              y2="64"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0%" stopColor="#34d399" />
+              <stop offset="50%" stopColor="#5eead4" />
+              <stop offset="100%" stopColor="#14b8a6" />
+            </linearGradient>
+          </defs>
+
+          <path
+            d="M10 26H6V38H10V26ZM18 22H14V42H18V22ZM26 30V26H22V38H26V34H36L30 40L34 44L48 30L34 16L30 20L36 26H26ZM50 22H46V42H50V22ZM58 26H54V38H58V26Z"
+            fill="url(#liftlogGradient)"
+          />
+        </svg>
+      </motion.div>
+    </div>
+  );
+}
