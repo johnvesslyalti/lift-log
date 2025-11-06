b66c39991d4b0bf3c75e3ffa67b6e8f4f92af65a
johnvesslyalti <altijohnvessly@gmail.com>
2025-11-06 15:17:17 +0530

    feat: update with the logo

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -22,8 +22,38 @@ export default function AppSidebar() {
 
   return (
     <Sidebar className="bg-neutral-900 text-white border-r border-white/10">
-      <SidebarHeader className="p-4">
+      <SidebarHeader className="p-4 gap-3">
+        <div className="flex items-center gap-2">
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 64 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              id="liftlogGradient"
+              x1="0"
+              y1="0"
+              x2="64"
+              y2="64"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0%" stopColor="#34d399" />
+              <stop offset="50%" stopColor="#5eead4" />
+              <stop offset="100%" stopColor="#14b8a6" />
+            </linearGradient>
+          </defs>
+
+          <path
+            d="M10 26H6V38H10V26ZM18 22H14V42H18V22ZM26 30V26H22V38H26V34H36L30 40L34 44L48 30L34 16L30 20L36 26H26ZM50 22H46V42H50V22ZM58 26H54V38H58V26Z"
+            fill="url(#liftlogGradient)"
+          />
+        </svg>
+
         <h1 className="text-xl font-bold tracking-wide">LiftLog</h1>
+      </div>
       </SidebarHeader>
 
       <SidebarContent className="flex flex-col gap-1 px-2 mt-2">
