1e39460a1b9f4116f740562c91beff922dd4fe6a
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-09 15:34:05 +0530

    feat: upgraded to the latest version of the prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,9 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
+const prisma = new PrismaClient({ adapter })
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
